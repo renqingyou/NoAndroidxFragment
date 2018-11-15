031255f4d944dd49fccaab184b6bb1ee9ae22aa7
renqingyou@sensorsdata.cn <renqingyou@sensorsdata.cn>
2018-11-15 14:14:33 +0800

    optimize code

--- app/src/main/java/com/example/renqingyou/noandroidxfragment/MainActivity.java
+++ app/src/main/java/com/example/renqingyou/noandroidxfragment/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.renqingyou.noandroidxfragment;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
--- app/src/main/java/com/example/renqingyou/noandroidxfragment/TestFragment.java
+++ app/src/main/java/com/example/renqingyou/noandroidxfragment/TestFragment.java
@@ -2,16 +2,15 @@ package com.example.renqingyou.noandroidxfragment;
 
 //import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.AppCompatButton;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.sensorsdata.analytics.android.sdk.SensorsDataAPI;
 import com.sensorsdata.analytics.android.sdk.SensorsDataTrackFragmentAppViewScreen;
 
 
