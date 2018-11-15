92d346730fdb245524414f96432b3bee485c6815
renqingyou@sensorsdata.cn <renqingyou@sensorsdata.cn>
2018-11-15 17:28:23 +0800

    add dialog

--- app/src/main/java/com/example/renqingyou/noandroidxfragment/MainActivity.java
+++ app/src/main/java/com/example/renqingyou/noandroidxfragment/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.renqingyou.noandroidxfragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -12,11 +14,132 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.bt1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"我是Actvity上的text",Toast.LENGTH_SHORT).show();
+                popDialog1();
             }
         });
     }
+
+    /**
+     * 提示对话框
+     */
+    public void popDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("问题：");
+        builder.setMessage("请问你满十八岁了吗?");
+        builder.setIcon(R.mipmap.ic_launcher_round);
+        //点击对话框以外的区域是否让对话框消失
+        builder.setCancelable(true);
+        //设置正面按钮
+        builder.setPositiveButton("是的", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "你点击了是的", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        //设置反面按钮
+        builder.setNegativeButton("不是", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "你点击了不是", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        //设置中立按钮
+        builder.setNeutralButton("保密", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "你选择了保密", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        //两种方式显示Dialog
+
+        /*AlertDialog dialog = builder.create();
+        //对话框显示的监听事件
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Log.e("rqy","对话框显示了");
+            }
+        });
+        //对话框消失的监听事件
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                Log.e("rqy","对话框消失了");
+            }
+        });
+        //显示对话框
+        dialog.show();*/
+
+        builder.show().setOwnerActivity(this);
+
+    }
+
+
+    /**
+     * 提示对话框
+     */
+    public void popDialog1() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("问题：");
+        builder.setMessage("请问你满十八岁了吗?");
+        builder.setIcon(R.mipmap.ic_launcher_round);
+        //点击对话框以外的区域是否让对话框消失
+        builder.setCancelable(true);
+        //设置正面按钮
+        builder.setPositiveButton("是的", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "你点击了是的", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        //设置反面按钮
+        builder.setNegativeButton("不是", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "你点击了不是", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        //设置中立按钮
+        builder.setNeutralButton("保密", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "你选择了保密", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        //两种方式显示Dialog
+
+        /*AlertDialog dialog = builder.create();
+        //对话框显示的监听事件
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Log.e("rqy","对话框显示了");
+            }
+        });
+        //对话框消失的监听事件
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                Log.e("rqy","对话框消失了");
+            }
+        });
+        //显示对话框
+        dialog.show();*/
+
+        builder.show().setOwnerActivity(this);
+
+    }
+
+
 }
