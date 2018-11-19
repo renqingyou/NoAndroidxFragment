407d4b94e9cda0a4b0789307731a947547909748
renqingyou@sensorsdata.cn <renqingyou@sensorsdata.cn>
2018-11-19 10:12:49 +0800

    add viewpager onPageSelected

--- app/src/main/java/com/example/renqingyou/noandroidxfragment/ViewPagerActivity.java
+++ app/src/main/java/com/example/renqingyou/noandroidxfragment/ViewPagerActivity.java
@@ -1,6 +1,8 @@
 package com.example.renqingyou.noandroidxfragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +17,28 @@ public class ViewPagerActivity extends AppCompatActivity {
         ViewPager mGuideVp = findViewById(R.id.vp_guide_guide);
         GuideAdapter mGuideAdapter = new GuideAdapter(this);
         mGuideVp.setAdapter(mGuideAdapter);
-        /*mGuideVp.setOnClickListener(new View.OnClickListener() {
+        mGuideVp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.e("rqy","viewPager");
             }
-        });*/
+        });
+
+        mGuideVp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                      Log.e("rqy","axxx");
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
