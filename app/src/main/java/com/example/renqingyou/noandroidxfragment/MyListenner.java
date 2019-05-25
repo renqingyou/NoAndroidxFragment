0ee74a62d578affe65749e568b84218f5b649db3
任庆友 <renqingyou@sensorsdata.cn>
2019-05-25 09:47:39 +0800

    save code

--- app/src/main/java/com/example/renqingyou/noandroidxfragment/MyListenner.java
+++ app/src/main/java/com/example/renqingyou/noandroidxfragment/MyListenner.java
@@ -4,6 +4,9 @@ import android.util.Log;
 
 public class MyListenner {
     public void test(A a){
+        new MyListenner().test(a1 -> {
+
+        });
         Log.e("rqy","a="+a);
     }
     public interface A {
