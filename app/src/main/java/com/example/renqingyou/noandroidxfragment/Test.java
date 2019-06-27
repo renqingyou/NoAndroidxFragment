560c13a104ac1b6eeb5f8adad07504797733b81d
任庆友 <renqingyou@sensorsdata.cn>
2019-06-27 18:03:58 +0800

    保存代码

--- app/src/main/java/com/example/renqingyou/noandroidxfragment/Test.java
+++ app/src/main/java/com/example/renqingyou/noandroidxfragment/Test.java
@@ -3,7 +3,27 @@ import android.view.View;
 
 public class Test {
 
-    public void a(View v) {
+    public static void main(){
+        Test test = new Test();
+        test.a(v -> {
+            myMethod(v);
+        });
+    }
 
+    public static void main1(){
+        Test test = new Test();
+        test.a(Test::myMethod);
+    }
+
+    public void a(MyListener listener) {
+
+    }
+
+    interface MyListener {
+        void click(View v);
+    }
+
+    public static void myMethod(View v) {
+        System.out.println("v");
     }
 }
